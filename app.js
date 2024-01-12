383ed368e526695923202d76c5c474d193129bd0
rbsingh001 <129167801+rbsingh001@users.noreply.github.com>
2024-01-12 19:35:49 +0530

    sign in work complete

--- app.js
+++ app.js
@@ -39,6 +39,33 @@ app.post('/user/signup', async (req, res) => {
     }
 });
 
+app.post('/user/login', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const existingUser = await User.findOne({
+            where: {
+                email: email
+            }
+        });
+        if(!existingUser){
+            return res.status(404).json({ message: 'User not found with this email' });
+        }
+       
+        if(existingUser.password === password){
+            return res.status(200).json({ message: 'Login successful' });
+        }
+        else{
+            return res.status(401).json({ message: 'Incorrect password' });
+        }
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 
 sequelize
     // .sync({force: true})
