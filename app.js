433b70f4f619ac4b6da3fda9a9aa3a040ed8938c
rbsingh001 <129167801+rbsingh001@users.noreply.github.com>
2024-01-15 14:15:04 +0530

    password saved encrypted to add security
    save password with bcrypt and also matching password with bcrypt .compare while logging in

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const sequelize = require('./utils/database');
 const User = require('./model/user');
+const bcrypt = require('bcrypt');
 
 var cors = require('cors');
 
@@ -22,16 +23,20 @@ app.post('/user/signup', async (req, res) => {
                 email: email
             }
         });
-        if(existingUser){
+        if (existingUser) {
             return res.status(409).send({ message: 'User with this email already exists' });
         }
-
-        const user = await User.create({
-            name: name,
-            email: email,
-            password: password
+        bcrypt.hash(password, 10, async (err, hash) => {
+            console.log(err);
+            const user = await User.create({
+                name: name,
+                email: email,
+                password: hash
+            })
+            res.send(user);
         })
-        res.send(user);
+
+
     }
     catch (err) {
         console.error(err);
@@ -49,16 +54,24 @@ app.post('/user/login', async (req, res) => {
                 email: email
             }
         });
-        if(!existingUser){
+        if (!existingUser) {
             return res.status(404).json({ message: 'User not found with this email' });
         }
-       
-        if(existingUser.password === password){
-            return res.status(200).json({ message: 'Login successful' });
+
+        else {
+            bcrypt.compare(password, existingUser.password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({ message: 'Error Occured' });
+                }
+                if (result === true) {
+                    return res.status(200).json({ message: 'Login successful' });
+                }
+                else{
+                    return res.status(200).json({ message: 'password not matched' });
+                }
+            })
         }
-        else{
-            return res.status(401).json({ message: 'Incorrect password' });
-        }
+
     }
     catch (err) {
         console.error(err);
